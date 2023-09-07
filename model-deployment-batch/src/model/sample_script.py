121e996e46c0f2c8aab27b1ae409160c0d7d1543
farbodtaymouri <farbodtaymouri@yahoo.com>
2023-09-07 15:34:26 +0000

    batch inference

---
+++ model-deployment-batch/src/model/sample_script.py
@@ -0,0 +1,12 @@
+
+# Sample script to run in the custom environment
+
+import sys
+import os
+import pickle
+import glob
+import pandas as pd
+from pathlib import Path
+from typing import List
+
+print(sys.version)
